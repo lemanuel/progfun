eaa17ec23b21c337fb9a51c6326c23590d6ca93e
emanuel l <lal.emanuel@gmail.com>
2016-06-10 15:30:20 +0200

    Finish Exercise 3, and rewrite exercise 2.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,24 +14,21 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
   
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    def iter(chars: List[Char], openParen: Int): Boolean =
-      if (chars.isEmpty)
-        true
-      else if (chars.head == '(')
-        iter(chars.tail, openParen + 1)
-      else if (chars.head == ')') {
-        if (openParen == 0)
-          false
-        else iter(chars.tail, openParen - 1)
-      } else
-        iter(chars.tail, openParen)
+    def iter(chars: List[Char], openParen: Int): Boolean = chars match {
+      case Nil => true
+      case '(' :: rest => iter(rest, openParen + 1)
+      case ')' :: rest if openParen == 0 => false
+      case ')' :: rest => iter(rest, openParen - 1)
+      case _ :: rest => iter(rest, openParen)
+    }
 
     iter(chars, 0)
   }
@@ -39,5 +36,9 @@ object Main {
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
+}
